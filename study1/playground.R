00afc499684576f687ddd55ec6e2976450f06ff5
Diogo Marques <diogohomemmarques@gmail.com>
2017-09-29 18:03:25 +0100

    code unfolding

--- study1/playground.R
+++ study1/playground.R
@@ -1,4 +1,13 @@
-# interactive manipulation
+stop("INTERATIVE - DO NOT SOURCE")
+
+source("study1/setup.R")
+
+# save/retrieve from vault
+saveProjectToVault()
+restoreProjectFromVault()
+
+# open project on RQDA
+RQDA()
 
 # search stories containing bathroom and shower
 View(searchFiles("file like '%shower%'", content = T))
