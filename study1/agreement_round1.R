71e49e661a8a4da13086502ad6aa2f539a322de9
Diogo Marques <diogohomemmarques@gmail.com>
2017-11-23 18:49:14 +0000

    manual code comparison w/ r3

--- study1/agreement_round1.R
+++ study1/agreement_round1.R
@@ -2,22 +2,27 @@ source("study1/agreement_setup.R")
 library(glue)
 library(irr)
 library(purrr)
+library(readr)
 
 ##
-# What's was the agreement after the first round of coding by raters 1 & 2?
+# What's was the agreement after the first round of coding?
 ## 
 
-## Compare first 10 codes between raters 1 & 2 manually
+## Out codes to CSV to compare manually (preserving rater comments)
 
 # 2nd rater codes (using responses clean to preserve comments)
 r2.codings.wide = 
   responses.clean %>% filter(rater == "ivan") %>% select(-timestamp)
 
+# 3rd rater codes
+r3.codings.wide = 
+  responses.clean %>% filter(rater == "tiago") %>% select(-timestamp)
+
 # 1st rater codes, in same format
 # TODO: removing then adding rater
 r1.codings.wide = 
   codings %>% 
-  filter(rater == "diogo" & fid <= 10) %>%
+  filter(rater == "diogo") %>%
   select(fid, codename) %>% 
   # regex magic to get category
   mutate(category = str_extract(codename, "^[a-z]+(?=\\-)")) %>%
@@ -26,11 +31,17 @@ r1.codings.wide =
   spread(category, codes) %>%
   mutate(rater = "diogo")
 
-both = bind_rows(r2.codings.wide, r1.codings.wide) %>%
-  filter(fid <= 10) %>%
+r1_vs_r2 = 
+  bind_rows(r2.codings.wide, r1.codings.wide) %>%
   arrange(fid, rater)
 
-write_csv(both, "out/s1_combined_raters_round1_1-10.csv")
+all = 
+  bind_rows(r1_vs_r2, r3.codings.wide) %>%
+  arrange(fid, rater)
+
+# TODO: this is somehow now fucked
+write_csv(r1_vs_r2, "out/s1_combined_r1_r2_round1_1-10.csv")
+write_csv(all, "out/s1_combined_all_round1_1-10.csv")
 
 ##
 # Calculate agreement scores
