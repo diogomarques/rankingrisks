700c5fea26965801149f37d2fc5d57fad0c02c48
Diogo Marques <diogohomemmarques@gmail.com>
2017-12-07 12:51:38 +0000

    single point for coding retrievals

--- study1/setup.R
+++ study1/setup.R
@@ -11,3 +11,39 @@ library(glue)
 RQDA_PROJECT_PATH = "study1/study1.rqda"
 RQDA_VAULT_NAME = "study1"
 SHEETS_VAULT = "sheets"
+
+# Obtain all codings per case from the RQDA database, annotated
+# category, code, subcode, ans respective descriptions
+getCodes = function(database = RQDA_PROJECT_PATH) {
+  # retrieve data from DB
+  RQDA::openProject(database)
+  codings = RQDA::getCodingTable()
+  category = RQDA::RQDAQuery("select name, memo from codecat where status = 1")
+  code = RQDA::RQDAQuery("select name, memo from freecode where status = 1")
+  RQDA::closeProject()
+  
+  # clean-up dara
+  codings = 
+    codings %>%
+    # extract category and code applying code name convention
+    mutate(category = str_extract(codename, "[a-z]*"),
+           code.subcode = str_extract(codename, "(?<=\\-)[:graph:]*"),
+           code = str_extract(codename, "(?<=\\-)[a-z]*"),
+           subcode = str_extract(code.subcode, "(?<=\\-)[a-z]*")
+    ) %>%
+    select(fid, codename, category, code, subcode)  
+  
+  # join to code descriptions
+  codings = 
+    codings %>% 
+    left_join(code, by=c("codename" = "name")) %>%
+    rename(description = memo)
+  
+  # join to category description
+  codings =
+    codings %>% 
+    left_join(category, by=c("category" = "name")) %>%
+    rename(category.desc = memo)
+  
+  codings
+}
