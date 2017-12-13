8d4e219a118557c969b44d5551407c688c917e07
Diogo Marques <diogohomemmarques@gmail.com>
2017-12-13 17:32:34 +0000

    funcs for retrieving gsheets data

--- R/retrieve_data.R
+++ R/retrieve_data.R
@@ -4,8 +4,6 @@
 RQDA_PROJECT_PATH = "data-raw/study1.rqda"
 SHEETS_VAULT = "sheets"
 
-## TODO: also centralize retrieval from GSheets here.
-
 #' Obtain all codings per case from the RQDA database, annotated
 #' with code categories, codes, subcodes, and respective descriptions
 #'
@@ -55,9 +53,84 @@ retrieve_rqda_codings = function(rqda_db = RQDA_PROJECT_PATH
     rename(category.desc = memo)
   
   # save
-  if(save = TRUE)
+  if(save == TRUE)
     save(codings, file = file.path("data", "codings.rda"))
   
   # return value
   codings
+}
+
+#' Get a list of Google Sheet keys stored in the vault.
+#'
+#' @param vault A hadley/secure vault containing keys to Google
+#' Sheet with relevant data.
+#'
+#' @return A list of keys
+#' @export
+#'
+#' @examples
+#' 
+get_sheet_keys = function(vault = SHEETS_VAULT) {
+  require(secure)
+  decrypt(SHEETS_VAULT)
+}
+
+#' Assure access to Google Sheets by running gs_ls() once. May require entering
+#' authorization code in command line if a valid oauth token is found.
+#'
+#' @return TRUE if there's access to Google Sheets.
+#' @export
+#'
+#' @examples
+#' has_acess_to_sheets()
+#' > httpuv not installed, defaulting to out-of-band authentication
+#' > Enter authorization code: xxxxx
+#' > [1] TRUE
+has_access_to_sheets = function() {
+  require(tidyverse)
+  require(googlesheets)
+  gs_ls() %>% is_tibble()
+}
+
+#' Retrieve data as-is from Google Sheet, given the name of the
+#' the key in the encrypted vault.
+#' 
+#' Access to Google Sheets, if not already available, may be asked
+#' for.
+#'
+#' @param sheet_name one of names(get_sheet_keys)
+#' @param vault A hadley/secure vault containing keys to Google
+#' Sheet with relevant data.
+#'
+#' @return A tibble containing the Google Sheet data
+#' @export
+#'
+#' @examples
+retrieve_sheet_data = function(sheet_name, vault = SHEETS_VAULT) {
+  require(glue)
+  require(tidyverse)
+  
+  # check that name is in vault
+  sheets = get_sheet_keys(vault = vault)
+  if (!(sheet_name %in% names(sheets)))
+    stop(glue("No sheet named {sheet_name} is stored in the {vault} vault."))
+  
+  # check for Google Sheet access
+  if(!has_access_to_sheets())
+    stop(glue("Access to Google Sheets not available."))
+  
+  # check for access to named Google Sheet
+  key = pluck(sheets, sheet_name)
+  matches = gs_ls() %>% 
+    filter(sheet_key == key) %>% 
+    count() %>% 
+    first()
+  if(matches != 1)
+    stop(glue("No sheet with given key available to you on 
+              Google Sheets."))
+  
+  # if all this passes, get sheet
+  key %>% 
+    gs_key() %>% # register
+    gs_read() # read
 }
